68563afd54e65b4254ee29be71c2bcdf89255ef6
NaganoKeita <keita0223n@gmail.com>
2017-06-16 14:54:24 +0900

    本の登録できるよ

--- src/controller/BookConfirmation.java
+++ src/controller/BookConfirmation.java
@@ -7,6 +7,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import beans.Book;
+import service.BookService;
 
 @WebServlet(urlPatterns = { "/book-confirm"})
 public class BookConfirmation extends HttpServlet{
@@ -17,5 +21,30 @@ public class BookConfirmation extends HttpServlet{
 			throws ServletException, IOException{
 
 		request.getRequestDispatcher("confirmation.jsp").forward(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException{
+
+		HttpSession session = request.getSession();
+
+		Book book = new Book();
+		String libraryId = (String) session.getAttribute("bookLibraryId");
+		book.setLibraryId(Integer.parseInt(libraryId));
+		String shelfId = (String) session.getAttribute("shelfId");
+		book.setShelfId(Integer.parseInt(shelfId));
+		book.setISBN((String) session.getAttribute("ISBN"));
+		book.setName((String) session.getAttribute("bookName"));
+		book.setAuthor((String) session.getAttribute("author"));
+		book.setPublisher((String) session.getAttribute("publisher"));
+		String kind = (String) session.getAttribute("kind");
+		book.setKind(Integer.parseInt(kind));
+
+		new BookService().register(book);
+
+		response.sendRedirect("admin");
+
+		session.invalidate();
 	}
 }
