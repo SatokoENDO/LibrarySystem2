a281612e4953fc2cd8625d8f6c613216e985313f
SatokoENDO <endo.satoko@m.alhinc.jp>
2017-06-16 16:55:36 +0900

    some bugs fixed

--- src/controller/ConfirmationServlet.java
+++ src/controller/ConfirmationServlet.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,16 +10,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import beans.User;
+import service.LibraryService;
 import service.UserService;
+import beans.Library;
+import beans.User;
 
 @WebServlet(urlPatterns = { "/user-confirm"})
-public class UserConfirmation extends HttpServlet{
+public class ConfirmationServlet extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException{
+		HttpSession session = request.getSession();
+
+		List<Library> libraries = new LibraryService().getLibraryList();
+		session.setAttribute("libraries", libraries);
 
 		request.getRequestDispatcher("confirmation.jsp").forward(request, response);
 	}
@@ -44,9 +51,11 @@ public class UserConfirmation extends HttpServlet{
 
 		new UserService().register(user);
 
+		session.invalidate();
+
 		response.sendRedirect("admin");
 
-		session.invalidate();
+
 
 	}
 
