09824028644b84be703dc941dcc09ecca77538d1
SatokoENDO <endo.satoko@m.alhinc.jp>
2017-06-19 17:48:08 +0900

    test

--- src/dao/BookDao.java
+++ src/dao/BookDao.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -115,6 +116,26 @@ public class BookDao {
 			return ret;
 		} finally {
 			close(rs);
+		}
+	}
+
+	//最新のid（一連番号）を取得
+	public int getBookId(Connection connection) {
+
+		try {
+			Statement statement = connection.createStatement();
+			String mySql = "select id from books WHERE  id=(SELECT MAX(id) FROM books )";
+			ResultSet rs = statement.executeQuery(mySql);
+
+			int bookId = 0;
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				bookId = id;
+			}
+			return bookId;
+
+		} catch (SQLException e) {
+			throw new SQLRuntimeException(e);
 		}
 	}
 
